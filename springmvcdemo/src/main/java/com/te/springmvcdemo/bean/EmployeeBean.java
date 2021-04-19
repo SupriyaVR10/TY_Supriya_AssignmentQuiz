87c5275821dfac119a1367daadc4c27b80748c25
SupriyaVR10 <supriya.vr1996@gmail.com>
2021-04-19 22:05:17 +0530

    springmvc project

---
+++ springmvcdemo/src/main/java/com/te/springmvcdemo/bean/EmployeeBean.java
@@ -0,0 +1,50 @@
+package com.te.springmvcdemo.bean;
+
+import java.util.Date; 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="employee_info")
+public class EmployeeBean {
+	@Id
+	@Column
+	private Integer id;
+	@Column
+	private String name;
+	@Column
+	private Date dob;
+	@Column
+	private String password;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
